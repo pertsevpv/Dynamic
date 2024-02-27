b74b4b5cd2789ef9515131b95ed8528334a7453a
pertsevpv <pertsevpv@yandex.ru>
2024-02-27 10:13:13 +0300

    lexer print

--- src/test/java/TestParsing.java
+++ src/test/java/TestParsing.java
@@ -3,6 +3,7 @@ import dynamic.parser.gen.DynaParser;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class TestParsing {
+
+  @Test
+  public void testLexing() {
+    String source = readFile("mergeSort.d");
+    Assertions.assertDoesNotThrow(() -> {
+      var lexer = new DynaLexer(CharStreams.fromString(source));
+      var tokenStream = new CommonTokenStream(lexer);
+      tokenStream.fill();
+      var allTokens = tokenStream.getTokens();
+      allTokens.forEach(this::printTokenInfo);
+    });
+  }
 
   @Test
   public void testParsing() {
@@ -30,6 +43,34 @@ public class TestParsing {
       return Files.readString(Path.of("src", "test", "resources", "examples", filename));
     } catch (IOException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private void printTokenInfo(Token token) {
+    if (token.getType() == Token.EOF) {
+      System.out.println("<EOF>");
+      return;
+    }
+    if (token.getChannel() == DynaLexer.HIDDEN) return;
+    var text = token.getText()
+        .replace("\n", "\\n")
+        .replace("\r", "\\r");
+    var ruleName = DynaLexer.ruleNames[token.getType() - 1];
+    System.out.print(ruleName + " at " + token.getLine() + ":" + token.getCharPositionInLine() + " — " + text + " ");
+    if (token.getChannel() == DynaLexer.HIDDEN) System.out.print("(HIDDEN)");
+    System.out.println();
+  }
+
+  private void printTokenNames(Token token) {
+    if (token.getType() == Token.EOF) {
+      System.out.println("<EOF>");
+      return;
+    }
+    if (token.getType() == DynaLexer.NEW_LINE || token.getType() == DynaLexer.WS)
+      System.out.print(token.getText());
+    else {
+      var ruleName = DynaLexer.VOCABULARY.getDisplayName(token.getType());
+      System.out.print(ruleName);
     }
   }
 
